566b124f3e07876d786a9c69956dacdb43b816ec
Night Chough <chicken.snuggles@gmail.com>
2021-06-12 09:47:46 -0500

    added some basic routes, matched model name to front-end

--- model/workout.js
+++ model/workout.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const exerciseSchema = new Schema({
+const workoutSchema = new Schema({
     name: {
         type: String,
         trim: true,
@@ -34,6 +34,6 @@ const exerciseSchema = new Schema({
     }
 });
 
-const Exercise = mongoose.model("Exercise", exerciseSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = Exercise;
+module.exports = Workout;
